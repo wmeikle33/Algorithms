1138da015f8a52bb3e944161e89fe8b230c2ce0c
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-08-15 17:16:05 +0800

    Update Fenwick Tree.py

--- Tree-Based Structures/Fenwick Tree.py
+++ Tree-Based Structures/Fenwick Tree.py
@@ -1,1 +1,18 @@
 
+class Fenwick:
+    def __init__(self, n: int):
+        self.n = n
+        self.bit = [0] * (n + 1)  
+
+    def add(self, i: int, delta: int) -> None:
+        print(delta)
+        while i <= self.n:
+            self.bit[i] += delta
+            i += i & -i
+
+    def sum(self, i: int) -> int:
+        s = 0
+        while i > 0:
+            s += self.bit[i]
+            i &= i - 1
+        return s
