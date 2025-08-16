102314d456cc6babe95457f4d3d45798b4056e13
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-08-16 09:33:09 +0800

    Update Segment Tree.py

--- Tree-Based Structures/Segment Tree.py
+++ Tree-Based Structures/Segment Tree.py
@@ -1,1 +1,42 @@
 
+class SegTreeSum:
+    __slots__ = ("n", "size", "tree")
+
+    def __init__(self, a: List[int]):
+        self.n = len(a)
+        self.size = 1 << (self.n - 1).bit_length()  # power of two
+        self.tree = [0] * (2 * self.size)
+        # build
+        self.tree[self.size:self.size + self.n] = a
+        for i in range(self.size - 1, 0, -1):
+            self.tree[i] = self.tree[2 * i] + self.tree[2 * i + 1]
+
+    def set(self, i: int, v: int) -> None:
+        """a[i] = v"""
+        p = i + self.size
+        self.tree[p] = v
+        p //= 2
+        while p:
+            self.tree[p] = self.tree[2 * p] + self.tree[2 * p + 1]
+            p //= 2
+
+    def add(self, i: int, delta: int) -> None:
+        """a[i] += delta"""
+        p = i + self.size
+        self.tree[p] += delta
+        p //= 2
+        while p:
+            self.tree[p] = self.tree[2 * p] + self.tree[2 * p + 1]
+            p //= 2
+
+    def sum(self, l: int, r: int) -> int:
+        """Sum over [l, r)"""
+        l += self.size; r += self.size
+        res = 0
+        while l < r:
+            if l & 1:
+                res += self.tree[l]; l += 1
+            if r & 1:
+                r -= 1; res += self.tree[r]
+            l //= 2; r //= 2
+        return res
