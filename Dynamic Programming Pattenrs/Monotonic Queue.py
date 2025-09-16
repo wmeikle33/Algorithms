4b654e95a5373aeeba7daa810001e32db3deb350
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-09-16 19:56:35 +0800

    Update Monotonic Queue.py

--- Dynamic Programming Pattenrs/Monotonic Queue.py
+++ Dynamic Programming Pattenrs/Monotonic Queue.py
@@ -4,19 +4,16 @@ from typing import List
 def constrained_subseq_sum(a: List[int], K: int) -> int:
     n = len(a)
     dp = [0]*n
-    dq = deque()  # stores indices j; dp[j] is decreasing
+    dq = deque() 
 
     for i in range(n):
-        # best previous dp in window
         best = dp[dq[0]] if dq else 0
         dp[i] = a[i] + max(0, best)
 
-        # maintain decreasing dp: pop smaller-than-new
         while dq and dp[dq[-1]] <= dp[i]:
             dq.pop()
         dq.append(i)
 
-        # remove out-of-window (j < i-K+1)
         while dq and dq[0] < i - K + 1:
             dq.popleft()
 
