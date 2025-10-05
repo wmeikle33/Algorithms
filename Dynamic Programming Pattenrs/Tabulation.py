364e9af8fc7a7ed6a3ae57b298e2f8f48a16fdd4
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-10-05 11:41:06 +0800

    Update Tabulation.py

--- Dynamic Programming Pattenrs/Tabulation.py
+++ Dynamic Programming Pattenrs/Tabulation.py
@@ -1,11 +1,10 @@
 INF = 10**15
 n = 100
 dp = [INF] * (n + 1)
-dp[0] = 0  # base
+dp[0] = 0 
 for i in range(1, n + 1):
     pass
 
-# 2D example
 m, n = 10, 10
 dp = [[0] * (n + 1) for _ in range(m + 1)]
 for i in range(1, m + 1):
