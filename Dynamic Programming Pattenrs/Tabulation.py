12bd5d6696e405b77a0b99bee85f728b4458c553
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-10-05 11:41:15 +0800

    Update Tabulation.py

--- Dynamic Programming Pattenrs/Tabulation.py
+++ Dynamic Programming Pattenrs/Tabulation.py
@@ -9,5 +9,4 @@ m, n = 10, 10
 dp = [[0] * (n + 1) for _ in range(m + 1)]
 for i in range(1, m + 1):
     for j in range(1, n + 1):
-        # dp[i][j] from dp[i-1][*], dp[*][j-1], etc.
         pass
