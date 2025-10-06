c7d4b85a3a2c42c693d49b7a9283c2d189105fe8
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-10-07 01:31:35 +0800

    Update Z Algorithm.py

--- String Techniques /Z Algorithm.py
+++ String Techniques /Z Algorithm.py
@@ -4,12 +4,9 @@ def z_function(s: str) -> list[int]:
     L = R = 0
     for i in range(1, n):
         if i <= R:
-            # mirror inside the current Z-box
             z[i] = min(R - i + 1, z[i - L])
-        # try to extend the match starting at i
         while i + z[i] < n and s[z[i]] == s[i + z[i]]:
             z[i] += 1
-        # update Z-box if we extended past R
         if i + z[i] - 1 > R:
             L, R = i, i + z[i] - 1
     return z
