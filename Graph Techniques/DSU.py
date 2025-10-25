952db67f08d0753dc6022a871476a9055107fb84
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-10-25 16:14:09 +0800

    Create DSU.py

---
+++ Graph Techniques/DSU.py
@@ -0,0 +1,21 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1]*n
+        self.sets = n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, a, b):
+        ra, rb = self.find(a), self.find(b)
+        if ra == rb:
+            return False
+        if self.size[ra] < self.size[rb]:
+            ra, rb = rb, ra
+        self.parent[rb] = ra
+        self.size[ra] += self.size[rb]
+        self.sets -= 1
+        return True
