509f393bdf23858b12b1f9dda3b135ef8a0fc61f
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-10-29 11:01:04 +0800

    Update Meet in the Middle.py

--- Search & Optimization Techniques/Meet in the Middle.py
+++ Search & Optimization Techniques/Meet in the Middle.py
@@ -1,13 +1,8 @@
 from itertools import combinations
 
 def all_subset_sums(arr):
-    """
-    Return a list of sums of all subsets of arr.
-    For arr of length m, this returns 2^m sums.
-    """
     sums = []
     n = len(arr)
-    # for each possible subset size k = 0..n
     for k in range(n + 1):
         for combo in combinations(arr, k):
             sums.append(sum(combo))
@@ -20,16 +15,12 @@ def subset_sum_meet_in_the_middle(nums, target):
     left = nums[:mid]
     right = nums[mid:]
 
-    # 1. all subset sums of left
-    left_sums = all_subset_sums(left)  # list of ints
+    left_sums = all_subset_sums(left)  
 
-    # 2. all subset sums of right
     right_sums = all_subset_sums(right)
 
-    # 3. Put left_sums into a set for O(1) lookup
     left_set = set(left_sums)
-
-    # 4. For each sum on the right, see if we can pair it
+    
     for rs in right_sums:
         need = target - rs
         if need in left_set:
