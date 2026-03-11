c2a99400a8f03b7ab92b4455cfcccba836643025
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-11 19:55:16 +0800

    Update Binary Lifting Example Problems.py

--- Problem Examples/Binary Lifting Example Problems.py
+++ Problem Examples/Binary Lifting Example Problems.py
@@ -1,5 +1,5 @@
 class LCA:
-  def __init__(self, n, edges, root = 1:
+  def __init__(self, n, edges, root = 1:)
     self.n = n
     self.Log = (n).bit_length()
     self.adj = [[] for _ in range(n+1)]
@@ -15,3 +15,20 @@ def _build(self, root):
   for i in range(1, self.LOG):
     self.up[root][i] = root
   stack = [(root,0)]
+  while stack:
+    u,p = stack.pop()
+    if p:
+      self.depth[u] = self.depth[p] + 1
+      self.up[u][0] = p
+      for i in range(1, self.LOG):
+        self.up[u][i] = self.up[self.up[u][i - 1]][i - 1]
+      for v in self.adj[u]:
+        if v == p:
+            continue
+        stack.append((v, u))
+def kth_ancestor(self, u, k):
+      for i in range(self.LOG):
+          if k & (1 << i):
+              u = self.up[u][i]
+      return u
+
